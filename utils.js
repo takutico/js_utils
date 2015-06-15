791441db90821243933b8e2fcec0894101fde431
typ <takutico@users.noreply.github.com>
2015-06-15 13:21:53 +0800

    Create utils.js

---
+++ utils.js
@@ -0,0 +1,7 @@
+function getParameterByName(name) {
+	/* function that get parameter from url */
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
