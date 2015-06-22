35c2aaba984c7fc027321052bd3891f733b968ee
typ <takutico@users.noreply.github.com>
2015-06-22 14:12:24 +0800

    Update utils.js

--- utils.js
+++ utils.js
@@ -5,3 +5,11 @@ function getParameterByName(name) {
         results = regex.exec(location.search);
     return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
+
+$( document ).ready( function() {
+	/* add class depending of image size */
+	$(".imgContainer").each(function(){
+        var imgClass = (this.width/this.height > 1) ? 'portrait' : 'landscape';
+        $(this).addClass(imgClass);
+	})
+});
